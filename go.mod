8104d056d21d88808d27ca229dea149e83158cce
Yusuf A. Hasan Miyan <yusufh@protonmail.com>
2022-04-11 11:41:29 +1000

    Added pflag.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yusufhm/rockpool
 
 go 1.17
+
+require github.com/spf13/pflag v1.0.5
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
+	"github.com/spf13/pflag"
 	r "github.com/yusufhm/rockpool/pkg/rockpool"
 )
 
@@ -10,15 +13,13 @@ var state r.State
 var config r.Config
 
 func main() {
+	config = r.Config{}
+	parseFlags()
+
 	state = r.State{}
 	r.VerifyReqs(&state)
 	fmt.Println()
 
-	config = r.Config{
-		ClusterName:   "rockpool",
-		LagoonBaseUrl: "lagoon.rockpool.k3d.local",
-		HarborPass:    "pass",
-	}
 	r.CreateCluster(&state, config.ClusterName)
 	fmt.Println()
 	r.GetClusterKubeConfigPath(&state, config.ClusterName)
@@ -30,4 +31,29 @@ func main() {
 
 	r.InstallHarbor(&state, &config)
 	r.InstallLagoonCore(&state, &config)
+}
+
+func parseFlags() {
+	pflag.ErrHelp = errors.New("rockpool: help requested")
+
+	pflag.Usage = func() {
+		fmt.Fprint(os.Stderr, "Rockpool\n\nEasily create local Lagoon instances.\n\n")
+		fmt.Fprintf(os.Stderr, "Usage:\n  %s [dir]\n\nFlags:\n", os.Args[0])
+		pflag.PrintDefaults()
+		fmt.Fprintln(os.Stderr)
+	}
+
+	displayUsage := pflag.BoolP("help", "h", false, "Displays usage information")
+	pflag.StringVarP(&config.ClusterName, "cluster-name", "n", "rockpool", "The name of the cluster")
+	pflag.StringVarP(&config.LagoonBaseUrl, "lagoon-base-url", "l", "lagoon.rockpool.k3d.local", `The base Lagoon url of the cluster;
+all Lagoon services will be created as subdomains of this url, e.g,
+ui.lagoon.rockpool.k3d.local, harbor.lagoon.rockpool.k3d.local`)
+	pflag.StringVar(&config.HarborPass, "harbor-password", "pass", "The Harbor password.")
+
+	pflag.Parse()
+
+	if *displayUsage {
+		pflag.Usage()
+		os.Exit(0)
+	}
 }
