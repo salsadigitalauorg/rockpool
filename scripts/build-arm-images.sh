78aade079aa5947dd49d6e6ce36e1585ecb04eb3
Yusuf A. Hasan Miyan <yusufh@protonmail.com>
2022-05-06 05:50:37 +1000

    feat: extended the image build script
    - ability to build single image
    - use vars for versions where possible
    - use further level for lagoon images

--- scripts/build-arm-images.sh
+++ scripts/build-arm-images.sh
@@ -2,29 +2,183 @@
 
 set -ex
 
-[ ! -d "build/keycloak-containers" ] && git clone https://github.com/keycloak/keycloak-containers.git build/keycloak-containers
-cd build/keycloak-containers && git checkout -- . && git clean -fd . && git checkout 7.0.1 && cd server
-docker build -t ghcr.io/yusufhm/rockpool/keycloak-arm:7.0.1 .
-docker push ghcr.io/yusufhm/rockpool/keycloak-arm:7.0.1
+ROCKPOOL_REPO=${ROCKPOOL_REPO:-https://github.com/yusufhm/rockpool}
+ROCKPOOL_IMAGES_REPO=${ROCKPOOL_IMAGES_REPO:-ghcr.io/yusufhm/rockpool}
+KEYCLOAK_VERSION=${KEYCLOAK_VERSION:-7.0.1}
+LAGOON_VERSION=${LAGOON_VERSION:-v2.5.0}
 
-cd ../../
-[ ! -d "lagoon" ] && git clone https://github.com/uselagoon/lagoon.git
-cd lagoon && git checkout -- . && git clean -fd . && git checkout v2.5.0 && cd services/keycloak
-sed -i .bak 's/jboss\/keycloak\:7\.0\.1/rockpool\/keycloak\-arm\:7\.0\.1/g' Dockerfile
-sed -i .bak 's/${TINI_VERSION}\/tini/${TINI_VERSION}\/tini\-arm64/g' Dockerfile
-sed -i .bak 's/\/var\/cache\/yum/\/var\/cache\/yum \&\& ln -s \/usr\/bin\/python2 \/usr\/bin\/python/g' Dockerfile
-docker build -t ghcr.io/yusufhm/rockpool/lagoon-keycloak-arm:v2.5.0 .
-docker push ghcr.io/yusufhm/rockpool/lagoon-keycloak-arm:v2.5.0
+function all () {
+  keycloak
+  lagoon
+  nfs_provisioner
+}
 
-cd ../broker-single
-docker build -t ghcr.io/yusufhm/rockpool/lagoon-broker-single-arm:v2.5.0 .
-docker push ghcr.io/yusufhm/rockpool/lagoon-broker-single-arm:v2.5.0
+# Build keycloak image.
+function keycloak () {
+  [ ! -d "keycloak-containers" ] && git clone https://github.com/keycloak/keycloak-containers.git keycloak-containers
+  pushd keycloak-containers && git checkout -- . && git clean -fd . && git checkout ${KEYCLOAK_VERSION} && pushd server
+  docker build \
+    --label "org.opencontainers.image.source=${ROCKPOOL_REPO}" \
+    --tag ${ROCKPOOL_IMAGES_REPO}/keycloak:${KEYCLOAK_VERSION} .
+  docker push ${ROCKPOOL_IMAGES_REPO}/keycloak:${KEYCLOAK_VERSION}
+  popd +2
+}
 
-cd ../broker
-sed -i .bak 's/${IMAGE_REPO\:\-lagoon}\/broker-single/ghcr.io\/yusufhm\/rockpool\/lagoon-broker-single-arm\:v2\.5\.0/g' Dockerfile
-docker build -t ghcr.io/yusufhm/rockpool/lagoon-broker:v2.5.0 .
-docker push ghcr.io/yusufhm/rockpool/lagoon-broker:v2.5.0
+function lagoon_clone () {
+  [ ! -d "lagoon" ] && git clone https://github.com/uselagoon/lagoon.git
+  pushd lagoon
+  git checkout -- . && git clean -fd . && git checkout ${LAGOON_VERSION}
+}
 
-cd ../../images/docker-host
-docker build -t ghcr.io/yusufhm/rockpool/lagoon-docker-host:v2.5.0 .
-docker push ghcr.io/yusufhm/rockpool/lagoon-docker-host:v2.5.0
+# Build lagoon images.
+function lagoon_keycloak () {
+  fetched=$1
+  if [ ! "${fetched}" == "true" ]; then
+    lagoon_clone
+  fi
+  pushd services/keycloak
+  sed -i .bak 's/jboss\/keycloak\:7\.0\.1/ghcr\.io\/yusufhm\/rockpool\/keycloak\:7\.0\.1/g' Dockerfile
+  sed -i .bak 's/${TINI_VERSION}\/tini/${TINI_VERSION}\/tini\-arm64/g' Dockerfile
+  sed -i .bak 's/\/var\/cache\/yum/\/var\/cache\/yum \&\& ln -s \/usr\/bin\/python2 \/usr\/bin\/python/g' Dockerfile
+  docker build \
+    --label "org.opencontainers.image.source=${ROCKPOOL_REPO}" \
+    --tag ${ROCKPOOL_IMAGES_REPO}/lagoon/keycloak:${LAGOON_VERSION} .
+  docker push ${ROCKPOOL_IMAGES_REPO}/lagoon/keycloak:${LAGOON_VERSION}
+  popd
+  if [ ! "${fetched}" == "true" ]; then
+    popd
+  fi
+}
+
+function lagoon_broker_single () {
+  fetched=$1
+  if [ ! "${fetched}" == "true" ]; then
+    lagoon_clone
+  fi
+  pushd services/broker-single
+  docker build \
+    --label "org.opencontainers.image.source=${ROCKPOOL_REPO}" \
+    --tag ${ROCKPOOL_IMAGES_REPO}/lagoon/broker-single:latest .
+  docker push ${ROCKPOOL_IMAGES_REPO}/lagoon/broker-single:latest
+  popd
+}
+
+function lagoon_broker () {
+  fetched=$1
+  if [ ! "${fetched}" == "true" ]; then
+    lagoon_clone
+  fi
+  pushd services/broker
+  docker build  \
+    --label "org.opencontainers.image.source=${ROCKPOOL_REPO}" \
+    --build-arg IMAGE_REPO=${ROCKPOOL_IMAGES_REPO}/lagoon \
+    --tag ${ROCKPOOL_IMAGES_REPO}/lagoon/broker:${LAGOON_VERSION} .
+  docker push ${ROCKPOOL_IMAGES_REPO}/lagoon/broker:${LAGOON_VERSION}
+  popd
+  if [ ! "${fetched}" == "true" ]; then
+    popd
+  fi
+}
+
+function lagoon_oc () {
+  fetched=$1
+  if [ ! "${fetched}" == "true" ]; then
+    lagoon_clone
+  fi
+  pushd images/oc
+  sed -i .bak 's/GLIBC_VERSION=2\.28\-r0/GLIBC_VERSION=2\.30\-r0/g' Dockerfile
+  sed -i .bak 's/sgerrand\/alpine-pkg-glibc/Rjerk\/alpine-pkg-glibc/g' Dockerfile
+  sed -i .bak 's/${GLIBC_VERSION}\//${GLIBC_VERSION}\-arm64\//g' Dockerfile
+  sed -i .bak 's/apk\ add\ glibc\-bin\.apk/apk\ add\ \-\-allow\-untrusted\ glibc\-bin\.apk/g' Dockerfile
+  docker build \
+    --label "org.opencontainers.image.source=${ROCKPOOL_REPO}" \
+    --tag ${ROCKPOOL_IMAGES_REPO}/lagoon/oc .
+  docker push ${ROCKPOOL_IMAGES_REPO}/lagoon/oc
+  popd
+  if [ ! "${fetched}" == "true" ]; then
+    popd
+  fi
+}
+
+function lagoon_auto_idler () {
+  fetched=$1
+  if [ ! "${fetched}" == "true" ]; then
+    lagoon_clone
+  fi
+  pushd services/auto-idler
+  docker build  \
+    --label "org.opencontainers.image.source=${ROCKPOOL_REPO}" \
+    --build-arg IMAGE_REPO=${ROCKPOOL_IMAGES_REPO}/lagoon \
+    --tag ${ROCKPOOL_IMAGES_REPO}/lagoon/auto-idler:${LAGOON_VERSION} .
+  docker push ${ROCKPOOL_IMAGES_REPO}/lagoon/auto-idler:${LAGOON_VERSION}
+  popd
+  if [ ! "${fetched}" == "true" ]; then
+    popd
+  fi
+}
+
+function lagoon_storage_calculator () {
+  fetched=$1
+  if [ ! "${fetched}" == "true" ]; then
+    lagoon_clone
+  fi
+  pushd services/storage-calculator
+  docker build \
+    --label "org.opencontainers.image.source=${ROCKPOOL_REPO}" \
+    --build-arg IMAGE_REPO=${ROCKPOOL_IMAGES_REPO}/lagoon \
+    --tag ${ROCKPOOL_IMAGES_REPO}/lagoon/storage-calculator:${LAGOON_VERSION} .
+  docker push ${ROCKPOOL_IMAGES_REPO}/lagoon/storage-calculator:${LAGOON_VERSION}
+  popd
+  if [ ! "${fetched}" == "true" ]; then
+    popd
+  fi
+}
+
+function lagoon_docker_host () {
+  fetched=$1
+  if [ ! "${fetched}" == "true" ]; then
+    lagoon_clone
+  fi
+  pushd images/docker-host
+  docker build \
+    --label "org.opencontainers.image.source=${ROCKPOOL_REPO}" \
+    --tag ${ROCKPOOL_IMAGES_REPO}/lagoon/docker-host:${LAGOON_VERSION} .
+  docker push ${ROCKPOOL_IMAGES_REPO}/lagoon/docker-host:${LAGOON_VERSION}
+  popd
+  if [ ! "${fetched}" == "true" ]; then
+    popd
+  fi
+}
+
+function lagoon () {
+  lagoon_clone
+  lagoon_keycloak true
+  lagoon_broker_single true
+  lagoon_broker true
+  lagoon_oc true
+  lagoon_auto_idler true
+  lagoon_storage_calculator true
+  lagoon_docker_host true
+  popd
+}
+
+# Build nfs-provisioner image.
+function nfs_provisioner () {
+  [ ! -d "nfs-provisioner" ] && git clone https://github.com/kubernetes-sigs/nfs-ganesha-server-and-external-provisioner.git nfs-provisioner
+  pushd nfs-provisioner && git checkout -- . && git clean -fd .
+  pushd cmd/nfs-provisioner
+  GOOS=linux GOARCH=arm64 go build -a -ldflags '-extldflags "-static"' -o ../../nfs-provisioner .
+  popd
+  docker build \
+    --label "org.opencontainers.image.source=${ROCKPOOL_REPO}" \
+    --tag ${ROCKPOOL_IMAGES_REPO}/nfs-provisioner:latest .
+  docker push ${ROCKPOOL_IMAGES_REPO}/nfs-provisioner:latest
+  popd
+}
+
+target=${1:-all}
+
+mkdir -p build
+cd build
+
+$target
