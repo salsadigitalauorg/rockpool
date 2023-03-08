a567098cd4c952e51d44c1b7389ceb17b2f081c2
Yusuf A. Hasan Miyan <yusufh@protonmail.com>
2023-03-08 14:18:21 +1100

    Set docker host image tag.

--- cmd/kube.go
+++ cmd/kube.go
@@ -12,40 +12,38 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var kubeConfigClusterControllerOnly bool
+var kubeConfigClusterTargetOnly int
 var kubeClusterControllerOnly bool
 var kubeClusterTargetOnly int
 var clusterNames []string
+var clusterName string
 
 var kubeCmd = &cobra.Command{
 	Use:   "kube [command]",
 	Short: "Execute kubernetes operations.",
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		k3d.ClusterFetch()
-		if kubeClusterControllerOnly || (!kubeClusterControllerOnly && kubeClusterTargetOnly == 0) {
-			clusterNames = append(clusterNames, platform.ControllerClusterName())
-		}
-		if kubeClusterControllerOnly {
-			return
-		}
-		if len(k3d.Clusters) > 1 {
-			for _, c := range k3d.Clusters {
-				if c.Name == platform.ControllerClusterName() {
-					continue
-				}
-				if kubeClusterTargetOnly > 0 &&
-					c.Name != platform.TargetClusterName(kubeClusterTargetOnly) {
-					continue
-				}
-				clusterNames = append(clusterNames, c.Name)
-			}
-		}
-	},
 }
 
 var kubeConfigCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Outputs the kubeconfig path for the cluster(s)",
 	PreRun: func(cmd *cobra.Command, args []string) {
+		if kubeConfigClusterControllerOnly {
+			clusterNames = append(clusterNames, platform.ControllerClusterName())
+			return
+		}
+		if kubeConfigClusterTargetOnly > 0 {
+			clusterNames = append(clusterNames, platform.TargetClusterName(kubeClusterTargetOnly))
+			return
+		}
+
+		k3d.ClusterFetch()
+		if len(k3d.Clusters) > 1 {
+			for _, c := range k3d.Clusters {
+				clusterNames = append(clusterNames, c.Name)
+			}
+		}
+
 		if len(clusterNames) == 0 {
 			log.Fatal("no cluster found")
 		}
@@ -62,62 +60,75 @@ var kubeCtlCmd = &cobra.Command{
 	Aliases: []string{"k"},
 	Short:   "Runs kubectl commands with the specified cluster",
 	PreRun: func(cmd *cobra.Command, args []string) {
-		if len(clusterNames) == 0 {
-			log.Info("no cluster specifed, setting to controller")
-			clusterNames = append(clusterNames, "controller")
+		if kubeClusterControllerOnly {
+			clusterName = platform.ControllerClusterName()
 		}
-		if len(clusterNames) > 1 {
-			log.Fatal("can run k9s on only 1 cluster at a time")
+		if kubeClusterTargetOnly > 0 {
+			clusterName = platform.TargetClusterName(kubeClusterTargetOnly)
+		}
+		if clusterName == "" {
+			log.Fatal("no cluster specified - use one of --controller or --target=1")
 		}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		kc := kube.KubeconfigPath(clusterNames[0])
+		k3d.ClusterFetch()
+		kc := kube.KubeconfigPath(clusterName)
 		command.Syscall("kubectl", append([]string{"--kubeconfig", kc}, args...))
 	},
 }
 var rootKubectlCmd = &cobra.Command{
-	Use:              kubeCtlCmd.Use,
-	Aliases:          kubeCtlCmd.Aliases,
-	Short:            kubeCtlCmd.Short,
-	PersistentPreRun: kubeCmd.PersistentPreRun,
-	PreRun:           kubeCtlCmd.PreRun,
-	Run:              kubeCtlCmd.Run,
+	Use:     kubeCtlCmd.Use,
+	Aliases: kubeCtlCmd.Aliases,
+	Short:   kubeCtlCmd.Short,
+	PreRun:  kubeCtlCmd.PreRun,
+	Run:     kubeCtlCmd.Run,
 }
 
 var kubeK9sCmd = &cobra.Command{
-	Use:   "k9s",
-	Short: "Runs k9s with the specified cluster",
-	PreRun: func(cmd *cobra.Command, args []string) {
-		if len(clusterNames) == 0 {
-			log.Info("no cluster specifed, setting to controller")
-			clusterNames = append(clusterNames, "controller")
-		}
-		if len(clusterNames) > 1 {
-			log.Fatal("can run k9s on only 1 cluster at a time")
-		}
-	},
+	Use:    "k9s",
+	Short:  "Runs k9s with the specified cluster",
+	PreRun: kubeCtlCmd.PreRun,
 	Run: func(cmd *cobra.Command, args []string) {
-		kc := kube.KubeconfigPath(clusterNames[0])
+		k3d.ClusterFetch()
+		kc := kube.KubeconfigPath(clusterName)
 		command.Syscall("k9s", []string{"--kubeconfig", kc})
 	},
 }
 var rootK9sCmd = &cobra.Command{
-	Use:              kubeK9sCmd.Use,
-	Short:            kubeK9sCmd.Short,
-	PersistentPreRun: kubeCmd.PersistentPreRun,
-	PreRun:           kubeK9sCmd.PreRun,
-	Run:              kubeK9sCmd.Run,
+	Use:    kubeK9sCmd.Use,
+	Short:  kubeK9sCmd.Short,
+	PreRun: kubeK9sCmd.PreRun,
+	Run:    kubeK9sCmd.Run,
 }
 
 func init() {
-	kubeCmd.PersistentFlags().BoolVar(&kubeClusterControllerOnly, "controller",
+	kubeConfigCmd.Flags().BoolVar(&kubeConfigClusterControllerOnly, "controller",
 		false, "Get controller cluster kubeconfig only")
-	kubeCmd.PersistentFlags().IntVar(&kubeClusterTargetOnly, "target",
+	kubeConfigCmd.Flags().IntVar(&kubeConfigClusterTargetOnly, "target",
+		0, "Get single target cluster kubeconfig")
+
+	kubeCtlCmd.Flags().BoolVar(&kubeClusterControllerOnly, "controller",
+		true, "Get controller cluster kubeconfig only")
+	kubeCtlCmd.Flags().IntVar(&kubeClusterTargetOnly, "target",
+		0, "Get single target cluster kubeconfig")
+	rootKubectlCmd.Flags().BoolVar(&kubeClusterControllerOnly, "controller",
+		true, "Get controller cluster kubeconfig only")
+	rootKubectlCmd.Flags().IntVar(&kubeClusterTargetOnly, "target",
+		0, "Get single target cluster kubeconfig")
+
+	kubeK9sCmd.Flags().BoolVar(&kubeClusterControllerOnly, "controller",
+		true, "Get controller cluster kubeconfig only")
+	kubeK9sCmd.Flags().IntVar(&kubeClusterTargetOnly, "target",
+		0, "Get single target cluster kubeconfig")
+	rootK9sCmd.Flags().BoolVar(&kubeClusterControllerOnly, "controller",
+		true, "Get controller cluster kubeconfig only")
+	rootK9sCmd.Flags().IntVar(&kubeClusterTargetOnly, "target",
 		0, "Get single target cluster kubeconfig")
 
 	kubeCmd.AddCommand(kubeConfigCmd)
 	kubeCmd.AddCommand(kubeCtlCmd)
 	kubeCmd.AddCommand(kubeK9sCmd)
+
 	rootCmd.AddCommand(kubeCmd)
 	rootCmd.AddCommand(rootKubectlCmd)
 	rootCmd.AddCommand(rootK9sCmd)
