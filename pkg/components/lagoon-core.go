ba2e2aba68ccc99604eac7cc2e92f2cb6e8e599c
Yusuf A. Hasan Miyan <yusufh@protonmail.com>
2025-02-18 18:58:45 +1100

    feat: ensure db tables are created for lagoon-core

--- pkg/components/lagoon-core.go
+++ pkg/components/lagoon-core.go
@@ -1,15 +1,18 @@
 package components
 
 import (
+	log "github.com/sirupsen/logrus"
+
 	"github.com/salsadigitalauorg/rockpool/pkg/action"
+	"github.com/salsadigitalauorg/rockpool/pkg/command"
 	"github.com/salsadigitalauorg/rockpool/pkg/config"
 	"github.com/salsadigitalauorg/rockpool/pkg/helm"
+	"github.com/salsadigitalauorg/rockpool/pkg/kube"
 	"github.com/salsadigitalauorg/rockpool/pkg/lagoon"
 )
 
 func init() {
 	Add("lagoon-core", func() Component {
-
 		lagoonValues := config.C.ToMap()
 		lagoonValues["LagoonVersion"] = lagoon.Version
 		return Component{
@@ -28,6 +31,35 @@ func init() {
 					Args:               []string{"--create-namespace", "--wait", "--timeout", "30m0s"},
 					ValuesTemplate:     "lagoon-core-values.yml.tmpl",
 					ValuesTemplateVars: lagoonValues,
+				},
+				action.Handler{
+					Info: "ensuring db tables have been created",
+					Func: func(logger *log.Entry) bool {
+						cn := logger.Data["cluster"].(string)
+
+						logger.Debug("checking if tables exist")
+						out, err := kube.Cmd(cn, "lagoon-core", "exec",
+							"sts/lagoon-core-api-db", "--", "bash", "-c",
+							"mysql -u$MARIADB_USER -p$MARIADB_PASSWORD $MARIADB_DATABASE -e 'SHOW TABLES;'",
+						).Output()
+						if err != nil {
+							logger.WithError(command.GetMsgFromCommandError(err)).
+								Fatal("error getting tables")
+						}
+						if string(out) != "" {
+							return true
+						}
+
+						logger.Debug("running the db init script")
+						err = kube.Cmd(cn, "lagoon-core", "exec", "sts/lagoon-core-api-db",
+							"--", "/legacy_rerun_initdb.sh").Run()
+						if err != nil {
+							logger.WithError(command.GetMsgFromCommandError(err)).
+								Fatal("error running db init")
+						}
+
+						return true
+					},
 				},
 			},
 		}
